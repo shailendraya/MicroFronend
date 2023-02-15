9405e90520dcbbdb538d8d843455ceea1cd9333f
shailendraya <shailendraya1988@gmail.com>
2023-02-15 15:36:24 +0530

    CI/CD yml file changes

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -5,6 +5,7 @@ import { StylesProvider } from '@material-ui/core/styles';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+
 export default () => {
   return (
     <div>
