1706220254cf776052eb93c7d38e2603efb044aa
shailendraya <shailendraya1988@gmail.com>
2023-02-20 14:13:22 +0530

    Micro frontend project completed

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -5,21 +5,25 @@ import {
   createGenerateClassName,
 } from '@material-ui/core/styles';
 
-import SignIn from './components/Signin';
-import SignUp from './components/Signup';
+import Signin from './components/Signin';
+import Signup from './components/Signup';
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'au',
 });
 
-export default ({ history }) => {
+export default ({ history, onSignIn }) => {
   return (
     <div>
       <StylesProvider generateClassName={generateClassName}>
         <Router history={history}>
           <Switch>
-            <Route  path="/auth/signin" component={SignIn} />
-            <Route path="/auth/signup" component={SignUp}/>I
+            <Route path="/auth/signin">
+              <Signin onSignIn={onSignIn} />
+            </Route>
+            <Route path="/auth/signup">
+              <Signup onSignIn={onSignIn} />
+            </Route>
           </Switch>
         </Router>
       </StylesProvider>
